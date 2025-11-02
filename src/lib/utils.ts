f02ea069356ca9ebd4613567e97aa7c5f9a7f742
muteekhan06 <muteekhan06@gmail.com>
2025-11-02 19:58:35 +0500

    fix(types): add speakeasy declaration and make debounce cross-platform (avoid NodeJS.Timeout)

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -47,9 +47,9 @@ export function debounce<T extends (...args: any[]) => any>(
   func: T,
   wait: number
 ): (...args: Parameters<T>) => void {
-  let timeout: NodeJS.Timeout;
+  let timeout: ReturnType<typeof setTimeout> | undefined;
   return (...args: Parameters<T>) => {
-    clearTimeout(timeout);
+    if (timeout !== undefined) clearTimeout(timeout);
     timeout = setTimeout(() => func(...args), wait);
   };
 }
